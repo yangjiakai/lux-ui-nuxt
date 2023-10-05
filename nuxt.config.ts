7eb078b60ddbf99a8714ce6523c0f34e3c6ca917
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-10-05 21:23:26 +0900

    初始化vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,21 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ["@/assets/main.scss"],
+  modules: [
+    async (options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        config.plugins ||= [];
+        config.plugins.push(vuetify());
+      });
+    },
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
 })
