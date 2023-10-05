d72d05878bc938271721770cbe83f7da7dd594da
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-10-06 03:32:18 +0900

    import google fonts

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -3,7 +3,7 @@
     <v-sheet class="layout-side d-none d-md-flex flex-column justify-space-between">
       <div class="mt-3 mt-md-10 pa-2">
         <div class="text-primary text-h4 font-weight-bold">
-          Lux Vuetify3 Admin
+          Lux Admin Nuxt3
         </div>
         <div class="title my-2">
           Welcome! Let's build amazing things together.
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,6 +23,18 @@ export default defineNuxtConfig({
           }));
       });
     },
+    ['@nuxtjs/google-fonts', {
+      families: {
+        Quicksand: true,
+        Inter: [400, 700],
+        'Josefin+Sans': true,
+        Lato: [100, 300],
+        Raleway: {
+          wght: [100, 400],
+          ital: [100]
+        },
+      }
+    }],
   ],
 
   vite: {
