700ba929cd06e6dafbed7fad214f213b651cf87c
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-10-23 21:29:50 +0900

    Add Router Change Transition

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,7 +7,6 @@
 import { RouterView } from "vue-router";
 import MainSiderbar from "@/components/app/main-sidebar/MainSiderbar.vue";
 import AppBar from "@/components/app/app-bar/AppBar.vue";
-
 </script>
 
 <style scoped lang="scss"></style>
@@ -18,10 +17,10 @@ import AppBar from "@/components/app/app-bar/AppBar.vue";
     <AppBar />
     <v-main class="fill-height">
       <div class="fill-height pa-5">
-        <RouterView />
+        <transition name="fade">
+          <RouterView />
+        </transition>
       </div>
-
     </v-main>
-
   </v-app>
 </template>
