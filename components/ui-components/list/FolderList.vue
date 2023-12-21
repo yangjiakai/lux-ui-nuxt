f4b6f10b8f03ecc4e34fd58c81933da95535c230
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-12-21 13:07:14 +0900

    Update FolderList.vue

--- components/ui-components/list/FolderList.vue
+++ components/ui-components/list/FolderList.vue
@@ -1,37 +1,38 @@
 <script setup lang="ts">
 import { Icon } from "@iconify/vue";
 const folders = ref([
-    {
-        subtitle: 'Jan 9, 2022',
-        title: 'Photos',
-        icon: 'FolderIcon',
-    },
-    {
-        subtitle: 'Jan 17, 2022',
-        title: 'Works',
-        icon: 'BriefcaseIcon'
-    },
-    {
-        subtitle: 'Jan 18, 2022',
-        title: 'Vacation',
-        icon: 'BeachIcon'
-    }
+  {
+    subtitle: "Jan 9, 2022",
+    title: "Photos",
+  },
+  {
+    subtitle: "Jan 17, 2022",
+    title: "Works",
+  },
+  {
+    subtitle: "Jan 18, 2022",
+    title: "Vacation",
+  },
 ]);
 </script>
 <template>
-    <v-list lines="two">
-        <v-card variant="outlined">
-            <v-list-item v-for="folder in folders" :key="folder.title" :subtitle="folder.subtitle">
-                <template v-slot:prepend>
-                    <Icon width="20" class="mr-3" icon="solar:album-line-duotone" />
-                </template>
-                <template v-slot:title>
-                    <h5 class="text-subtitle-1">{{ folder.title }}</h5>
-                </template>
-                <template v-slot:subtitle>
-                    <span class="text-subtitle-2">{{ folder.subtitle }}</span>
-                </template>
-            </v-list-item>
-        </v-card>
-    </v-list>
+  <v-list lines="two">
+    <v-card variant="outlined">
+      <v-list-item
+        v-for="folder in folders"
+        :key="folder.title"
+        :subtitle="folder.subtitle"
+      >
+        <template v-slot:prepend>
+          <Icon width="20" class="mr-3" icon="solar:album-line-duotone" />
+        </template>
+        <template v-slot:title>
+          <h5 class="text-subtitle-1">{{ folder.title }}</h5>
+        </template>
+        <template v-slot:subtitle>
+          <span class="text-subtitle-2">{{ folder.subtitle }}</span>
+        </template>
+      </v-list-item>
+    </v-card>
+  </v-list>
 </template>
