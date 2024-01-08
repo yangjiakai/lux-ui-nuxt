e7a5788d46e100de72795d29eeb5a37bda715365
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2024-01-08 23:33:34 +0900

    Create Base.vue

---
+++ pages/table/Base.vue
@@ -0,0 +1,137 @@
+<!--
+* @Component: 
+* @Maintainer: J.K. Yang
+* @Description: 
+-->
+<script setup lang="ts">
+const page = ref(1);
+const itemsPerPage = ref(5);
+const headers = ref([
+  {
+    align: "start",
+    key: "name",
+    sortable: false,
+    title: "Dessert (100g serving)",
+  },
+  { title: "Calories", key: "calories" },
+  { title: "Fat (g)", key: "fat" },
+  { title: "Carbs (g)", key: "carbs" },
+  { title: "Protein (g)", key: "protein" },
+  { title: "Iron (%)", key: "iron" },
+]);
+const desserts = ref([
+  {
+    name: "Frozen Yogurt",
+    calories: 159,
+    fat: 6,
+    carbs: 24,
+    protein: 4,
+    iron: 1,
+  },
+  {
+    name: "Ice cream sandwich",
+    calories: 237,
+    fat: 9,
+    carbs: 37,
+    protein: 4.3,
+    iron: 1,
+  },
+  {
+    name: "Eclair",
+    calories: 262,
+    fat: 16,
+    carbs: 23,
+    protein: 6,
+    iron: 7,
+  },
+  {
+    name: "Cupcake",
+    calories: 305,
+    fat: 3.7,
+    carbs: 67,
+    protein: 4.3,
+    iron: 8,
+  },
+  {
+    name: "Gingerbread",
+    calories: 356,
+    fat: 16,
+    carbs: 49,
+    protein: 3.9,
+    iron: 16,
+  },
+  {
+    name: "Jelly bean",
+    calories: 375,
+    fat: 0,
+    carbs: 94,
+    protein: 0,
+    iron: 0,
+  },
+  {
+    name: "Lollipop",
+    calories: 392,
+    fat: 0.2,
+    carbs: 98,
+    protein: 0,
+    iron: 2,
+  },
+  {
+    name: "Honeycomb",
+    calories: 408,
+    fat: 3.2,
+    carbs: 87,
+    protein: 6.5,
+    iron: 45,
+  },
+  {
+    name: "Donut",
+    calories: 452,
+    fat: 25,
+    carbs: 51,
+    protein: 4.9,
+    iron: 22,
+  },
+  {
+    name: "KitKat",
+    calories: 518,
+    fat: 26,
+    carbs: 65,
+    protein: 7,
+    iron: 6,
+  },
+]);
+const pageCount = computed(() => {
+  return Math.ceil(desserts.value.length / itemsPerPage.value);
+});
+</script>
+
+<template>
+  <v-data-table
+    v-model:page="page"
+    :headers="headers"
+    :items="desserts"
+    :items-per-page="itemsPerPage"
+  >
+    <template v-slot:top>
+      <v-text-field
+        :model-value="itemsPerPage"
+        class="pa-2"
+        hide-details
+        label="Items per page"
+        min="-1"
+        max="15"
+        type="number"
+        @update:model-value="itemsPerPage = parseInt($event, 10)"
+      ></v-text-field>
+    </template>
+
+    <template v-slot:bottom>
+      <div class="text-center pt-2">
+        <v-pagination v-model="page" :length="pageCount"></v-pagination>
+      </div>
+    </template>
+  </v-data-table>
+</template>
+
+<style scoped lang="scss"></style>
