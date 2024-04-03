40b9b88c65371c46262e76f2aad29c3d2dd86bbf
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2024-04-03 21:34:03 +0900

    Update InfiniteScroll.vue

--- components/animation/InfiniteScroll.vue
+++ components/animation/InfiniteScroll.vue
@@ -9,4 +9,15 @@
   <div class=""></div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.carousel-container {
+  overflow: hidden;
+}
+.carousel {
+  display: flex;
+  transition: transform 0.5s ease;
+}
+.carousel-item {
+  min-width: 100%;
+}
+</style>
